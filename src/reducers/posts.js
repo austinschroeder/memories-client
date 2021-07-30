b48f491545f85f103ee8a40cb00e89bacebf5770
austinschroeder <austinsoundy@gmail.com>
2021-07-30 14:59:44 -0700

    Added memory form, and file submit

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,9 +1,9 @@
 export default (posts = [], action) => {
   switch (action.type) {
     case 'FETCH_ALL':
-      return state;
+      return action.payload;
     case 'CREATE':
-      return state;
+      return posts;
     default:
       return posts;
   }
