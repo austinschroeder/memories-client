4983d530c4573383bfaa35af0b84c7d60b2c2062
austinschroeder <austinsoundy@gmail.com>
2021-07-30 16:46:57 -0700

    Start of card functionality

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -19,7 +19,7 @@ const Post = ({ post }) => {
       </div>
       <div className={classes.overlay2}>
         <Button style={{color: 'white'}} size="small" onClick={() => {}}>
-          <MoreHorizIcon fontSize="default" />
+          <MoreHorizIcon fontSize="medium" />
         </Button>
       </div>
       <div className={classes.details}>
