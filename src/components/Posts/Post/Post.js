4c26bdb0ed9dcc412bc4afe2c8ce7a2e2554c51a
austinschroeder <austinsoundy@gmail.com>
2021-08-11 14:19:29 -0600

    Edit functionality is now working

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -29,8 +29,9 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
       <CardContent>
-        <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+        <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
       </CardContent>
+      <Typography variant="h5" gutterBottom>{post.message}</Typography>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" onClick={() => { }}>
           <ThumbUpAltIcon fonstSize="small" />
