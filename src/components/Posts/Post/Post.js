570a0f3a98ad258935347191411c93234ba070ef
austinschroeder <austinsoundy@gmail.com>
2021-08-15 17:30:13 -0700

    Delete functionality is working

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,11 +4,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts'
 
 import useStyles from './styles';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <Card className={classes.card}>
@@ -38,7 +42,7 @@ const Post = ({ post, setCurrentId }) => {
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => { }}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fonstSize="small" />
           Delete
         </Button>
