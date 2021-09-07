56e0003e6513f4510265a0e6b7c2a8a410080768
austinschroeder <austinsoundy@gmail.com>
2021-09-07 10:27:16 -0700

    Fix typo

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <Container maxwidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h2' align='center' v>Things to remember</Typography>
+        <Typography className={classes.heading} variant='h2' align='center'>Things to remember</Typography>
         <img className={classes.image} src={memories} alt='memories' height='60' />
       </AppBar>
       <Grow in>
